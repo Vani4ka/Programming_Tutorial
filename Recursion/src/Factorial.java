57a9efae5a73cba1c3201906a7c8ffde8a2fc637
Vani4ka <vanya.gercheva@uni-weimar.de>
2017-05-03 14:57:54 +0200

    Recursion/Factorial added

--- Recursion/src/Factorial.java
+++ Recursion/src/Factorial.java
@@ -1,4 +1,24 @@
+import java.util.Scanner;
+
 
 public class Factorial {
+
+	public static int factorial (int n) {
+		int sum=1;
+		for(int i=1; i<=n; i++) {
+			sum *= i;
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Please enter your number: ");
+		int number = scan.nextInt();
+
+		System.out.println("Factorial of " + number + " is "+ factorial(number));
+
+	}
 	//todo: implement a recursive functions whicht gives you the factorial value of a given number
 }
